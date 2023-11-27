db12d253e30417c0488b7f4182f873fdfa4dd8d5
subtlemocha00 <kturley@gmail.com>
2023-11-27 09:49:22 -0500

    styled search bar

--- react_projects/weather-app/src/Header.jsx
+++ react_projects/weather-app/src/Header.jsx
@@ -1,10 +1,26 @@
 export default function Header() {
 	return (
-		<div>
-			<input className="form-control" type="text"></input>
-			<button>
-				<i className="btn fas fa-search"></i>
-			</button>
-		</div>
+		<section className="p-3 bg-secondary text-center text-warning">
+			<h3>SkyScape</h3>
+			<div className="input-group p-3 bg-secondary m-0">
+				<div className="form-outline"></div>
+				<input
+					id="searchInput"
+					className="form-control"
+					type="search"
+					placeholder="Search..."
+				></input>
+				<label className="form-label sr-only" htmlFor="searchInput">
+					Search
+				</label>
+				<button
+					id="searchBtn"
+					type="button"
+					className="btn btn-warning text-secondary"
+				>
+					<i className="fas fa-magnifying-glass "></i>
+				</button>
+			</div>
+		</section>
 	);
 }
